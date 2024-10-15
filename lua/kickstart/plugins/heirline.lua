a0c030ad5d87c253eeea2bd67a27f2134f8f6af0
koodihirmu <tomvar98@gmail.com>
2024-10-15 20:37:19 +0300

    heirline working

--- lua/kickstart/plugins/heirline.lua
+++ lua/kickstart/plugins/heirline.lua
@@ -1,87 +1,88 @@
 return {
   'rebelot/heirline.nvim',
-  -- You can optionally lazy-load heirline on UiEnter
-  -- to make sure all required plugins and colorschemes are loaded before setup
-  event = 'UiEnter',
-  config = function()
-    local utils = require 'heirline.utils'
-    local conditions = require 'heirline.conditions'
-
-    local colors = {
-      bright_bg = utils.get_highlight('Folded').bg,
-      bright_fg = utils.get_highlight('Folded').fg,
-      red = utils.get_highlight('DiagnosticError').fg,
-      dark_red = utils.get_highlight('DiffDelete').bg,
-      green = utils.get_highlight('String').fg,
-      blue = utils.get_highlight('Function').fg,
-      gray = utils.get_highlight('NonText').fg,
-      orange = utils.get_highlight('Constant').fg,
-      purple = utils.get_highlight('Statement').fg,
-      cyan = utils.get_highlight('Special').fg,
-      diag_warn = utils.get_highlight('DiagnosticWarn').fg,
-      diag_error = utils.get_highlight('DiagnosticError').fg,
-      diag_hint = utils.get_highlight('DiagnosticHint').fg,
-      diag_info = utils.get_highlight('DiagnosticInfo').fg,
-      git_del = utils.get_highlight('diffDeleted').fg,
-      git_add = utils.get_highlight('diffAdded').fg,
-      git_change = utils.get_highlight('diffChanged').fg,
+  dependencies = { 'zeioth/heirline-components.nvim' },
+  opts = function()
+    local lib = require 'heirline-components.all'
+    return {
+      opts = {
+        disable_winbar_cb = function(args) -- We do this to avoid showing it on the greeter.
+          local is_disabled = not require('heirline-components.buffer').is_valid(args.buf)
+            or lib.condition.buffer_matches({
+              buftype = { 'terminal', 'prompt', 'nofile', 'help', 'quickfix' },
+              filetype = { 'NvimTree', 'neo%-tree', 'dashboard', 'Outline', 'aerial' },
+            }, args.buf)
+          return is_disabled
+        end,
+      },
+      tabline = { -- UI upper bar
+        lib.component.tabline_conditional_padding(),
+        lib.component.tabline_buffers(),
+        lib.component.fill { hl = { bg = 'tabline_bg' } },
+        lib.component.tabline_tabpages(),
+      },
+      winbar = { -- UI breadcrumbs bar
+        init = function(self)
+          self.bufnr = vim.api.nvim_get_current_buf()
+        end,
+        fallthrough = false,
+        -- Winbar for terminal, neotree, and aerial.
+        {
+          condition = function()
+            return not lib.condition.is_active()
+          end,
+          {
+            lib.component.neotree(),
+            lib.component.compiler_play(),
+            lib.component.fill(),
+            lib.component.compiler_build_type(),
+            lib.component.compiler_redo(),
+            lib.component.aerial(),
+          },
+        },
+        -- Regular winbar
+        {
+          lib.component.neotree(),
+          lib.component.compiler_play(),
+          lib.component.fill(),
+          lib.component.breadcrumbs(),
+          lib.component.fill(),
+          lib.component.compiler_redo(),
+          lib.component.aerial(),
+        },
+      },
+      statuscolumn = { -- UI left column
+        init = function(self)
+          self.bufnr = vim.api.nvim_get_current_buf()
+        end,
+        lib.component.foldcolumn(),
+        lib.component.numbercolumn(),
+        lib.component.signcolumn(),
+      } or nil,
+      statusline = { -- UI statusbar
+        hl = { fg = 'fg', bg = 'bg' },
+        lib.component.mode(),
+        lib.component.git_branch(),
+        lib.component.file_info(),
+        lib.component.git_diff(),
+        lib.component.diagnostics(),
+        lib.component.fill(),
+        lib.component.cmd_info(),
+        lib.component.fill(),
+        lib.component.lsp(),
+        lib.component.compiler_state(),
+        lib.component.virtual_env(),
+        lib.component.nav(),
+        lib.component.mode { surround = { separator = 'right' } },
+      },
     }
+  end,
+  config = function(_, opts)
+    local heirline = require 'heirline'
+    local heirline_components = require 'heirline-components.all'
 
-    require('heirline').load_colors(colors)
-
-    local FileType = {
-      provider = function()
-        return string.upper(vim.bo.filetype)
-      end,
-      hl = { fg = utils.get_highlight('Type').fg, bold = false },
-    }
-
-    local Space = {
-      provider = ' | ',
-      hl = { fg = 'blue', bold = true },
-    }
-
-    local WorkDir = {
-      provider = function()
-        local icon = (vim.fn.haslocaldir(0) == 1 and 'l' or 'g') .. ' ' .. ' '
-        local cwd = vim.fn.getcwd(0)
-        cwd = vim.fn.fnamemodify(cwd, ':~')
-        if not conditions.width_percent_below(#cwd, 0.25) then
-          cwd = vim.fn.pathshorten(cwd)
-        end
-        local trail = cwd:sub(-1) == '/' and '' or '/'
-        return icon .. cwd .. trail
-      end,
-      hl = { fg = 'blue', bold = true },
-    }
-
-    local LSPActive = {
-      condition = conditions.lsp_attached,
-      update = { 'LspAttach', 'LspDetach' },
-
-      -- You can keep it simple,
-      -- provider = " [LSP]",
-
-      -- Or complicate things a bit and get the servers names
-      provider = function()
-        local names = {}
-        for i, server in pairs(vim.lsp.get_clients { bufnr = 0 }) do
-          table.insert(names, server.name)
-        end
-        return ' [' .. table.concat(names, ' ') .. ']'
-      end,
-      hl = { fg = 'green', bold = true },
-    }
-
-    -- local StatusLine = { Ruler }
-    local WinBar = { { Space }, { WorkDir }, { Space }, { LSPActive } }
-    -- local TabLine = {}
-    -- local StatusColumn = {}
-    require('heirline').setup {
-      -- statusline = StatusLine,
-      winbar = WinBar,
-      -- tabline = TabLine,
-      -- statuscolumn = StatusColumn,
-    }
+    -- Setup
+    heirline_components.init.subscribe_to_events()
+    heirline.load_colors(heirline_components.hl.get_colors())
+    heirline.setup(opts)
   end,
 }
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -21,6 +21,7 @@ require('lazy').setup({
   require 'kickstart.plugins.treesitter-config', -- Parser generator tool for updating syntax as the source is edited
   require 'kickstart.plugins.lazygit',
   require 'kickstart.plugins.cmake_tools',
+  require 'kickstart.plugins.heirline',
   { 'kylechui/nvim-surround', version = '*', event = 'VeryLazy', config = true },
   { 'Bilal2453/luvit-meta', lazy = true },
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } }, -- Highlight todo, notes, etc in comments
