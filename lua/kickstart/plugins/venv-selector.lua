9b7ae19bb33cc6f950ab2a574cff841653a2a8fa
koodihirmu <tomvar98@gmail.com>
2024-10-15 20:25:50 +0300

    venv double required removed

--- lua/kickstart/plugins/venv-selector.lua
+++ lua/kickstart/plugins/venv-selector.lua
@@ -8,10 +8,6 @@ return {
   },
   lazy = false,
   branch = 'regexp', -- This is the regexp branch, use this for the new version
-  config = function()
-    require('venv-selector').setup()
-  end,
-
   opts = {
     -- Your options go here
     name = { 'venv', '.venv' },
